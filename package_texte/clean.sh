1f02c9f7c7044828102962f6fcd3ed525894432c
MrEli <mreli.pro@laposte.net>
2023-01-26 15:19:59 +0100

    Update clean.sh way faster

--- package_texte/clean.sh
+++ package_texte/clean.sh
@@ -1,40 +1,35 @@
-#!/bin/bash 
-
-#script pour creer les fichiers clean et tok avant descr.
+#!/bin/bash
 
 echo "This is $0"
 
 ARG=$1
- 
-array=$(cat ./package_texte/stopword.txt) 
-iconv -f ISO-8859-1 -t UTF-8 "$ARG" | sed 's/<\/\?[^>]\+>//g' |  tr -d '[:punct:]' | tr -s '[:space:]' | tr '[:upper:]' '[:lower:]' | tail -n +4 > "$ARG.pre"
 
-sed 's/-//g' "$ARG.pre" | sed 's/\./ /g' |  tr "\n" " " > "$ARG.clean"
-#cat "$ARG.clean"
+stopwords="./package_texte/stopword.txt"
 
+#convertir le fichier d'entrée en UTF-8 et on enregistre le résultat dans un nouveau fichier
+iconv -f ISO-8859-1 -t UTF-8 "$ARG" > "$ARG.utf8"
 
-#nous avons crée un fichier clean contenant sur une même ligne 
-#le fichier sans les balises, sans ponctuation 
-#et les majusccule transformés en minuscule
-
-#ensuite on enlève les stopword contenu dans le fichier ayant le meme nom
-
-
-
-#array=( mimer un le la pour par les ou )
-for i in ${array[@]}
-do
-    sed -i.bak -e  "s/\<$i\>//g" "$ARG.clean" 
-
-done >> "$ARG.tok"
-
-#cat "$ARG.clean"
-#cat "$ARG.tok"
-rm "$ARG.pre" "$ARG.clean"
-
-mv "$ARG.clean.bak" "$ARG.tok"
+# On utilise awk pour supprimer les balises XML du fichier converti en UTF-8
+# On lit chaque ligne du fichier de stopwords et on ajoute chaque mot à un tableau stopwords_set en minuscule
+awk '{gsub(/<[^>]*>/, ""); print $0}' "$ARG.utf8" | awk -v stopwords="$stopwords" '
+BEGIN {
+    FS = "[^a-zA-ZÀ-ÿ0-9]+"
+    while (getline < stopwords) {
+        stopwords_set[tolower($1)] = 1
+    }
+}
+{
+    for (i = 1; i <= NF; i++) {
+        if (!stopwords_set[tolower($i)]) {
+            printf("%s ", $i)
+        }
+    }
+    printf("\n")
+}
+' > "$ARG.tok"
 
 mv "$ARG.tok" ./package_texte/TOK
 
+rm "$ARG.utf8"
 
 echo "This is $0"
