15f1cdd21607d23ef5fa624729a1584da4ad47fa
Elio Genson <elio.genson@gmail.com>
2023-03-16 15:21:07 +0100

    Update comparaison.h

--- package_texte/comparaison.h
+++ package_texte/comparaison.h
@@ -10,7 +10,6 @@
 
 #include "text_descriptor_gen.h"
 
-
 #define MAX_TOKEN 2000
 #define MAX_TOKEN_LENGTH 50
 #define MAX_PARAMETER_LENGTH 10
@@ -21,35 +20,35 @@
 #define MENU__CHOIX_RECHERCHE_ID 2
 #define BASE_DESCRIPTEUR "./package_texte/base_descripteur_texte.txt"
 
-typedef struct {
+typedef struct
+{
   int nb_occurence;
   char keyword[MAX_TOKEN_LENGTH]; // It should be less than MAX_TOKEN_LENGTH
-}
-Token;
+} Token;
 
-typedef struct {
+typedef struct
+{
   int id;
   int nb_token;
   int nb_total_token;
-  Token * tokens;
-}
-Descripteur;
+  Token *tokens;
+} Descripteur;
 
-typedef struct {
+typedef struct
+{
   int id;
   int nb_occurence;
-}
-SearchComparableItem;
+} SearchComparableItem;
 
-typedef struct {
+typedef struct
+{
   int id;
   float similarite;
-}
-SimilariteTexte; 
+} SimilariteTexte;
 
-Descripteur * parse_base_descripteur(FILE * fp, int * descripteur_nb);
+Descripteur *parse_base_descripteur(FILE *fp, int *descripteur_nb);
 
-void search_by_keyword(char * mot_cle, Descripteur * descripteurs, int descripteurs_length);
+void search_by_keyword(char *mot_cle, Descripteur *descripteurs, int descripteurs_length);
 
 float cosine_similarity(Descripteur desc1, Descripteur desc2);
 
@@ -57,11 +56,11 @@ void comparaison_par_fichier(Descripteur descripteur);
 
 int get_new_document_id();
 
-int indexation_fichier(char * path_texte, int id);
+int indexation_fichier(char *path_texte, int id);
 
-Descripteur * get_descripteur_par_id(int id);
+Descripteur *get_descripteur_par_id(int id);
 
-char * get_path_by_id(int id);
+char *get_path_by_id(int id);
 
 void MENU__research_by_path();
 
